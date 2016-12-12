91361f94a60333aceebebe5d4534ae76da4414e0
Catherine Gracey <catherine.gracey@gmail.com>
2016-12-13 07:22:08 +1100

    Copy and routes for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'pages#index'
+  post '/register', to: 'users#create'
+  post '/login', to: 'users#login'
+  delete '/logout', to: 'users#logout'
 end
