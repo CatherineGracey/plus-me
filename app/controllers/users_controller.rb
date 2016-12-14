1e2ca71e65b0aae1292161e7e27fac30883da1f2
Catherine Gracey <catherine.gracey@gmail.com>
2016-12-14 18:09:59 +1100

    Users can update their details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,37 @@
 class UsersController < ApplicationController
 
   def create
-    create_user
-    redirect_to '/profile'
+    if params[:password] == params[:password_repeat]
+      user = User.find_by(email: params[:email])
+      if !user
+        user = User.new
+        user.email = params[:email]
+        user.username = params[:username]
+        user.password = params[:password]
+        user.save
+        session[:user_id] = user.id
+        redirect_to '/profile'
+      elsif user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to '/profile'
+      else
+        redirect_to '/'
+      end
+    else
+      redirect_to '/'
+    end
   end
 
   def login
-    @login_error = create_session
+    user = User.find_by(email: params[:email])
+    if !user
+      @login_error =  "Please check your email address and try again."
+    elsif user.authenticate(params[:password])
+      session[:user_id] = user.id
+      @login_error =  false
+    else
+      @login_error =  "Please check your password and try again."
+    end
     if @login_error
       redirect_to '/'
     else
@@ -15,15 +40,24 @@ class UsersController < ApplicationController
   end
 
   def logout
-    destroy_session
+    session[:user_id] = nil
     redirect_to '/'
   end
 
   def profile
+    @user = User.find(session[:user_id])
   end
 
   def update
-    update_user
+    if params[:id].to_i == session[:user_id].to_i
+      user = User.find(session[:user_id])
+      user.first_name = params[:first_name]
+      user.last_name = params[:last_name]
+      user.age = params[:age]
+      user.gender = params[:gender]
+      user.save
+    end
+    redirect_to '/profile'
   end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,38 +1,10 @@
 module UsersHelper
 
+  def current
+    User.find_by(email: params[:email])
+  end
+
   def logged_in?
-
-  end
-
-  def create_user
-    user = User.find_by(email: params[:email])
-    if !user
-      user = User.new
-      user.save
-    elsif user.authenticate(params[:password])
-      redirect_to '/profile'
-    else
-    end
-  end
-
-  def create_session
-    user = User.find_by(email: params[:email])
-    # Function is returning errors rather than successes
-    if !user
-      return "Please check your email address and try again."
-    elsif user.authenticate(params[:password])
-      session[:user_id] = user.id
-      return false
-    else
-      return "Please check your password and try again."
-    end
-  end
-
-  def destroy_session
-    session[:user_id] = nil
-  end
-
-  def update_user
 
   end
 
