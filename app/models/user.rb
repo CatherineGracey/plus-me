b4a7c9ff5904fb019483dd0f9005eba56f3e9e4b
Catherine Gracey <catherine.gracey@gmail.com>
2016-12-15 21:31:32 +1100

    Added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :username, uniqueness: true
+  validates :username, presence: true
   has_many :events
   has_many :attendees
 end
